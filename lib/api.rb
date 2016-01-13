bfdec5d71463180ac028e1fcc197068e2ec49e9b
zotherstupidguy <zotherstupidguy@gmail.com>
2016-01-13 16:04:33 +0200

    fixed the url retun for user

--- lib/api.rb
+++ lib/api.rb
@@ -46,7 +46,7 @@ module Gitlapse
       else
 	send(r[0],r[1])
 	p "Visit your lapse at: "
-	p "https://gitlapse.com/sample.html?SHA=#{r[0]}"
+	p "https://www.gitlapse.com/sample.html?SHA=#{r[0]}"
       end
     end
 
